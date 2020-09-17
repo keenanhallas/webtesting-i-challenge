056b26d52380f7f25629431dd53a308d33977fab
Keenan <keenanhallas@gmail.com>
2020-09-16 19:03:06 -0500

    all tests written

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,45 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 20) {
+    return {
+      ...item,
+      enhancement: item.enhancement + 1
+    };
+  } else {
+    return {...item};
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  let newItem;
+  if(item.enhancement < 15) {
+    newItem = {
+      ...item,
+      durability: item.durability - 5
+    };
+  } else {
+    newItem = {
+      ...item,
+      durability: item.durability - 10
+    };
+  }
+
+  if(item.enhancement > 16) {
+    return {
+      ...newItem,
+      enhancement: newItem.enhancement - 1
+    };
+  } else {
+    return newItem;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,95 @@
+const { exportAllDeclaration } = require('@babel/types');
+const { italic } = require('chalk');
+const { describe } = require('yargs');
 const enhancer = require('./enhancer.js');
 // test away!
+
+// describe("Testing item enhancer functions", () => {
+//     it.todo("- Write tests");
+// });
+
+it.todo("- Write tests");
+
+it("Tests the repair function", () => {
+    const item = {
+        name: "Short sword",
+        durability: 50,
+        enhancement: 0
+    };
+    console.log(item);
+    const repairedItem = enhancer.repair(item);
+    console.log(repairedItem);
+    expect(repairedItem.durability).toBe(100);
+});
+
+it("Tests enhancement success", () => {
+    const item = {
+        name: "Short sword",
+        durability: 50,
+        enhancement: 0
+    };
+    console.log(item);
+    const enhancedItem = enhancer.success(item);
+    console.log(enhancedItem);
+    expect(enhancedItem.enhancement).toBe(1);
+});
+
+it("Tests items with max enhancement aren't altered", () => {
+    const item = {
+        name: "Short sword",
+        durability: 50,
+        enhancement: 20
+    };
+    console.log(item);
+    const enhancedItem = enhancer.success(item);
+    console.log(enhancedItem);
+    expect(enhancedItem.enhancement).toBe(20);
+});
+
+it("Tests failure with enhancement under 15", () => {
+    const item = {
+        name: "Short sword",
+        durability: 100,
+        enhancement: 14
+    };
+    console.log(item);
+    const failedItem = enhancer.fail(item);
+    console.log(failedItem);
+    expect(failedItem.durability).toBe(95);
+});
+
+it("Tests failure with enhancement at 15", () => {
+    const item = {
+        name: "Short sword",
+        durability: 100,
+        enhancement: 15
+    };
+    console.log(item);
+    const failedItem = enhancer.fail(item);
+    console.log(failedItem);
+    expect(failedItem.durability).toBe(90);
+});
+
+it("Tests that enhancement doesn't change with enhancement at 16", () => {
+    const item = {
+        name: "Short sword",
+        durability: 100,
+        enhancement: 16
+    };
+    console.log(item);
+    const failedItem = enhancer.fail(item);
+    console.log(failedItem);
+    expect(failedItem.enhancement).toBe(16);
+});
+
+it("Tests that enhancement is lowered with enhancement over 16", () => {
+    const item = {
+        name: "Short sword",
+        durability: 100,
+        enhancement: 17
+    };
+    console.log(item);
+    const failedItem = enhancer.fail(item);
+    console.log(failedItem);
+    expect(failedItem.enhancement).toBe(16);
+});
